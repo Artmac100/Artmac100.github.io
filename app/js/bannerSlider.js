314a17d5d42edecfd308a3392940fd1b704c496c
Artem <artmakatera@gmail.com>
2017-10-03 17:21:00 +0300

    mae accordion and slider

---
+++ app/js/bannerSlider.js
@@ -0,0 +1,71 @@
+;(function($) {
+	$.fn.bannerSlider = function(options) {
+		var options = options || {} ;
+		var settings = $.extend({
+			slideWidth: 979,
+			slideHeight: 'inherit',
+			wrapperTarget: "img",
+			mode: 'horizontal',
+			duration: 1000,
+			// styles for arrows
+			slideLeft: '#prev',
+			slideRight: '#next'
+		}, options);
+		var self = this;
+		// css styles
+		$(self).wrap('<div class="slider-wrapper"><div class="slider-viewport"></div></div>');
+		var wrapper = $(self).parents('.slider-wrapper');
+		wrapper.css({
+			"margin": "0 auto",
+			"display": "flex",
+			"max-width": settings.slideWidth + "px",
+			"height": settings.slideHeight + "px",
+			"overflow": "hidden",
+			"margin": "0 auto"
+		})
+		$(settings.wrapperTarget, self).wrap('<div class="slider-item"></div>');
+		var slideItem = $('.slider-item').css( {
+			"width": settings.slideWidth  + "px",
+			"height": settings.slideHeight  + "px"
+		});
+		$(settings.wrapperTarget).css({
+			"max-width": settings.slideWidth,
+			"max-height": settings.slideHeight,
+			"margin": "0 auto"
+		});
+		// Set css parametrs horizontal aligning for slider's items
+		$(self).css({"width": "9999px "});
+		slideItem.css({ "float": "left","display": "inline-flex"});
+		// invoking of slide function
+		sliding(settings.slideWidth, "margin-left");
+		function sliding(heightOrWidth, slideTool) {
+			// margin slide by default
+			var ms = 0;
+			// define max sliding steps
+			var maxmr = -((slideItem.length - 1) * heightOrWidth);
+			function slideAnimation() {
+				// creating animation object
+				var objAnim = {};
+				objAnim[slideTool] = ms +"px";
+				$(self).animate(objAnim, settings.duration);
+			}
+			$(settings.slideLeft).on("click", function(event) {
+				event.preventDefault();
+				ms += heightOrWidth;
+				if (ms > 0) {
+					ms = maxmr;
+				}
+				slideAnimation();
+
+			});
+			$(settings.slideRight).on("click", function(event) {
+				event.preventDefault();
+				ms -= heightOrWidth;
+				if (ms < maxmr) {
+					ms = 0;
+				}
+				slideAnimation();
+			});
+		}
+	}
+})(jQuery);
---
+++ app/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$('.accordion-menu a').on('click', function(e) {
+		e.preventDefault();
+		console.log($(this).children())
+		$(this).siblings('ul').toggle();
+		$(this).children('i').toggleClass('accord-arrow-right');
+		$(this).children('i').toggleClass('accord-arrow-down');
+
+	});
+	$('.inner-banner').bannerSlider({
+		wrapperTarget: '.banner-item',
+		slideLeft: '#prev-slide',
+		slideRight: '#next-slide'
+	});
+
+	displayDropdown('.header__topmenu li', 'ul.dropdown ')
+	displayDropdown('.dropdown li', 'ul.dropdown-submenu')
+
+	function displayDropdown(parentSelector, childSelector) {
+		$(parentSelector).on('click', function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+			if($(this).has(childSelector)) {
+				$(this).siblings().children(childSelector).css('display', 'none');
+				if(parentSelector === '.dropdown li') $(this).siblings().removeClass('active-dropdown');
+				$(this).children(childSelector).toggle('display', 'block');
+				if(parentSelector === '.dropdown li') $(this).toggleClass('active-dropdown');
+			}
+		});
+	}
+});
+
